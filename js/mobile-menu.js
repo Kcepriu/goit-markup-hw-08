926f0881836199490d79398bef05955d114ff57c
Kcepriu <kcepriu@gmail.com>
2022-10-10 22:31:15 +0300

    change images

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,12 +2,14 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const bodyScrollLock = document.querySelector("body");
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    bodyScrollLock.classList.toggle('no-scroll');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -24,5 +26,6 @@
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
+    bodyScrollLock.classList.remove('no-scroll');
   });
 })();
